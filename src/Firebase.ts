9def952ddb6f2536fff8e8fbb26541f59cc417e1
Maksym Topolya <148054104+MaksymTopolya@users.noreply.github.com>
2024-08-01 12:44:18 +0300

    main

--- src/Firebase.ts
+++ src/Firebase.ts
@@ -5,7 +5,7 @@ import 'firebase/compat/firestore';
 
 const firebaseConfig = {
   apiKey: 'AIzaSyC9tK1sZp_5QtMscA5Aqfe0RUdeETqPc_U',
-  authDomain: 'movie-617c6.firebaseapp.com',
+  authDomain: 'https://filmik-dc89a.web.app',
   projectId: 'movie-617c6',
   storageBucket: 'movie-617c6.appspot.com',
   messagingSenderId: '938317420353',
