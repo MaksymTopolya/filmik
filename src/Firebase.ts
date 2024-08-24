f26de6b8b25c651c5e45caf00a5e1184b290e21d
Maksym Topolya <148054104+MaksymTopolya@users.noreply.github.com>
2024-08-24 13:36:36 +0300

    213

--- src/Firebase.ts
+++ src/Firebase.ts
@@ -1,5 +1,4 @@
 import firebase from 'firebase/compat/app';
-import 'firebase/firestore';
 import 'firebase/compat/auth';
 import 'firebase/compat/firestore';
 
@@ -13,7 +12,10 @@ const firebaseConfig = {
   measurementId: 'G-C0CSM087XJ',
 };
 
-firebase.initializeApp(firebaseConfig);
+
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig);
+}
 
 export const auth = firebase.auth();
 export const firestore = firebase.firestore();
