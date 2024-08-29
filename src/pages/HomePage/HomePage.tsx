ec9529b3df8641471482eb9baf401167596d1231
Maksym Topolya <148054104+MaksymTopolya@users.noreply.github.com>
2024-08-29 09:37:02 +0300

    123

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -2,40 +2,32 @@ import { FC } from 'react';
 import MovieSlider from '../../components/MovieSlider/MovieSlider';
 import NewMovies from '../../components/NewMovies/NewMovies';
 
-
-
-
-
-function App :FC() {
+const HomePage: FC = () => {
   const savePageAs = () => {
     const htmlContent = document.documentElement.outerHTML;
 
-   
     const blob = new Blob([htmlContent], { type: "text/html" });
 
     const link = document.createElement("a");
 
     link.href = URL.createObjectURL(blob);
 
-   
     link.download = "Filmik.html";
 
     link.click();
     URL.revokeObjectURL(link.href);
   };
 
-
-
   return (
     <div>
-        <div className="App">
-      <h1>Save Page As Example</h1>
-      <button onClick={savePageAs}>Save Page As</button>
-    </div>
+      <div>
+        <h1>Save Page As Example</h1>
+        <button onClick={savePageAs}>Save Page As</button>
+      </div>
       <MovieSlider />
       <NewMovies />
     </div>
   );
 };
 
-export default App;
+export default HomePage;
