c5a0ad933403ee84898f87844dffe4ca64432c71
Maksym Topolya <148054104+MaksymTopolya@users.noreply.github.com>
2024-09-09 14:12:38 +0300

    main

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -3,28 +3,8 @@ import MovieSlider from '../../components/MovieSlider/MovieSlider';
 import NewMovies from '../../components/NewMovies/NewMovies';
 import MobileDownload from '../../components/MobileDownload/MobileDownload';
 const HomePage: FC = () => {
-  const savePageAs = () => {
-    const htmlContent = document.documentElement.outerHTML;
-
-    const blob = new Blob([htmlContent], { type: "text/html" });
-
-    const link = document.createElement("a");
-
-    link.href = URL.createObjectURL(blob);
-
-    link.download = "Filmik.html";
-
-    link.click();
-    URL.revokeObjectURL(link.href);
-  };
-
   return (
     <div>
-      <div>
-        <h1>Save Page As Example</h1>
-        <button onClick={savePageAs}>Save Page As</button>
-      </div>
-      <MobileDownload/>
       <MovieSlider />
       <NewMovies />
     </div>
